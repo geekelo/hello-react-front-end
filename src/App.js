a445e8660c04ca53f3beee2e5cbcbfed0de9edd5
geekelo <efueliht12@gmail.com>
2023-11-20 19:46:29 +0100

    Add link to rails app in readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Greeting from './components/greetings';
+import Home from './components/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/greeting">Greeting</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+  
+
+      <Routes>
+        <Route path="/home" exact element={ <Home /> } />
+        <Route path="/greeting" element={ <Greeting /> } />
+        </Routes>
+        </Router>
+
     </div>
   );
 }
